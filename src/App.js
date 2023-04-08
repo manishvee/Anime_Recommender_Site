631355929b6f851a136f54f1cfaa5eadaa3a0979
AnishAdkikar <anishadkikar1411@gmail.com>
2023-04-08 13:36:39 +0530

    Firebase initilized

--- src/App.js
+++ src/App.js
@@ -3,40 +3,53 @@ import './styles/App.css';
 import Header from './components/Header.js';
 import TopAiring from './components/TopAiring';
 import Searched from './components/Searched';
+import db from './components/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 function App() {
+  const [userData, setUserData] = useState([]);
   const [topAnime, SetTopAnime] = useState([]);
   const [animeList, SetAnimeList] = useState([]);
-  const [search, SetSearch] = useState("");
-  const [searchStatus,SetSearchStatus] = useState(true); 
-  
+  const [search, SetSearch] = useState('');
+  const [searchStatus, SetSearchStatus] = useState(true);
 
   const GetTopAnime = async () => {
-    const response = await fetch('https://api.jikan.moe/v4/top/anime').then((res) => res.json());
-    SetTopAnime(response.data) 
-  }
-	const HandleSearch = e => {
-		e.preventDefault();
-    SetSearchStatus(false); 
-		FetchAnime(search);
-	}
-	const FetchAnime = async (query) => {
-		const response = await fetch(`https://api.jikan.moe/v4/anime?q=${query}&limit=10`)
-			.then(res => res.json());
-      console.log(query); 
-		SetAnimeList(response.data);
-	}
+    const response = await fetch('https://api.jikan.moe/v4/top/anime').then(
+      (res) => res.json()
+    );
+    SetTopAnime(response.data);
+  };
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    SetSearchStatus(false);
+    FetchAnime(search);
+  };
+  const FetchAnime = async (query) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${query}&limit=10`
+    ).then((res) => res.json());
+    console.log(query);
+    SetAnimeList(response.data);
+  };
 
-  
-  
+  const getUsers = async () => {
+    const collectionRef = collection(db, 'Users');
+    const querySnapshot = await getDocs(collectionRef);
+    setUserData(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
+  };
 
-
-  useEffect(()=>{
+  useEffect(() => {
+    getUsers();
     GetTopAnime();
-  },[]) 
-
+    return () => {
+      getUsers();
+      GetTopAnime();
+    };
+  }, []);
+  console.log(userData);
 
   return (
     <div className='App'>
+      <h2>{userData.length !== 0 && userData[0].name}</h2>
       <Header
         HandleSearch={HandleSearch}
         search={search}
